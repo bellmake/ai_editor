d44ee5ba05d3ca028ccdc81bc493d5affb51a6f8
joseph <bellmake@naver.com>
2024-07-02 12:25:16 +0900

    Streamlit segmentation test app

--- testapp2.py
+++ testapp2.py
@@ -1,17 +1,41 @@
 import streamlit as st
 from PIL import Image
 from PIL import ImageOps
+from streamlit_drawable_canvas import st_canvas
 
-import torch
 import numpy as np
 import cv2
+import torch
 from matplotlib import pyplot as plt
 from segment_anything import sam_model_registry, SamAutomaticMaskGenerator, SamPredictor
 
-from streamlit_drawable_canvas import st_canvas
+@st.cache_resource
+def get_sam():
+    device = torch.device(device='cuda' if torch.cuda.is_available() else 'cpu')
+    sam = sam_model_registry["vit_h"](checkpoint="/home/joseph/study/multimodal/ai_editor/sam_vit_h_4b8939.pth")
+    sam = sam.to(device)
+    
+    generator = SamAutomaticMaskGenerator(sam)
+    
+    return generator
+
+def show_anns(anns, ax):
+    if len(anns) == 0:
+        return
+    sorted_anns = sorted(anns, key=(lambda x: x['area']), reverse=True)
+    ax.set_autoscale_on(False)
+    
+    img = np.ones((sorted_anns[0]['segmentation'].shape[0], sorted_anns[0]['segmentation'].shape[1], 4))
+    img[:,:,3] = 0
+    for ann in sorted_anns:
+        m = ann['segmentation']
+        color_mask = np.concatenate([np.random.random(3), [0.35]])
+        img[m] = color_mask
+    ax.imshow(img)
+
 
 def main():
-    st.title('welcome to the segmentation world')
+    st.title('Segmentation Streamlit web test')
     torch.cuda.empty_cache()
     with st.sidebar:
         upload = st.file_uploader('insert image',type=['png','jpg'])
@@ -19,7 +43,7 @@ def main():
         sam_button = st.button('generate mask')
         
     if upload:
-        image = Image.open(upload)
+        image = Image.open(upload).convert('RGB')
         image = ImageOps.exif_transpose(image)
         
         image = np.array(image)
@@ -34,7 +58,14 @@ def main():
         
     if sam_button:
         generator = get_sam()
+        masks = generator.generate(st.session_state['image'])
+        torch.cuda.empty_cache()
         
+        fig, ax = plt.subplots(figsize=(20,20))
+        ax.imshow(image)
+        show_anns(masks, ax)
+        ax.axis('off')
+        st.pyplot(fig)
         
         
 if __name__ == '__main__':
